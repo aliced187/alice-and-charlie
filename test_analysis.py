4d18c41611127893879cfccab6a650616f6b0889
Alice Durand <ad2112@cam.ac.uk>
2022-02-28 21:42:07 +0000

    Analysis test

---
+++ test_analysis.py
@@ -0,0 +1,22 @@
+from distutils.command.build import build
+from os import times
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem import analysis
+from floodsystem.datafetcher import fetch_measure_levels
+from matplotlib import dates
+import numpy as np 
+import pytest
+import datetime
+
+stations = build_station_list()
+update_water_levels(stations)
+
+def test():
+    dt = 10
+    times, levels = fetch_measure_levels(stations.measure_id, dt = datetime.timedelta(days=dt))
+    x = dates.date2num(times)
+
+    poly, d0 = analysis.polyfit(times, levels, 2)
+
+    assert d0 == x[0]
+    assert isinstance(poly, np.poly1d)
