f4e0505acbaf2b47c919ec7dac14751b205ccc84
Charlie Kelly <cmk72@cam.ac.uk>
2022-03-01 08:23:29 +0000

    update taskg

--- Task2G.py
+++ Task2G.py
@@ -20,20 +20,18 @@ def run():
     for l in sevlist:
         namessev.append(l[0])
     print("Severe Flood risk ( current relative water level > 1): " + str(namessev))
+    #judged to be highest of risks because the water is above the highest value
     hlist = stations_level_over_threshold(stations, 0.8)
     for a in hlist:
         nameshigh.append(a[0])
     nameshighf = set(nameshigh) - set(namessev)
-    print("High Flood risk ( current relative water level > 0.8 ):" + str(nameshighf))
     mlist = stations_level_over_threshold(stations, 0.7)
     for k in mlist:
         namesmod.append(k[0])
     namesmodf = set(namesmod) - set(nameshigh)
-    print("Moderate Flood risk ( current relative water level > 0.7 ):" + str(namesmodf))
     for station in stations:
         names.append(station.name)
     nameslow = set(names) - set(namesmod)
-    print("Low Flood risk ( current relative water level <= 0.7 ):" + str(nameslow))
 
 
 run()
